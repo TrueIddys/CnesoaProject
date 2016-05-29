4f2638e54c220c240b847e7042e64a0b238a02b2
Maxime PEYRAL <maxime.peyral@gmail.com>
2016-05-30 00:03:30 +0200

    without postgresql properties

--- cnesoa/src/main/java/com/cnesoa/config/BuildSearchIndex.java
+++ cnesoa/src/main/java/com/cnesoa/config/BuildSearchIndex.java
@@ -1,34 +1,34 @@
-package com.cnesoa.config;
-
-import org.hibernate.search.jpa.FullTextEntityManager;
-import org.hibernate.search.jpa.Search;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationListener;
-import org.springframework.stereotype.Component;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-/**
- * Created by Maxime on 10/05/2016.
- */
-
-@Component
-public class BuildSearchIndex implements ApplicationListener{
-
-    @PersistenceContext
-    private EntityManager entityManager;
-
-
-    @Override
-    public void onApplicationEvent(ApplicationEvent applicationEvent) {
-        try {
-            FullTextEntityManager fullTextEntityManager =
-                    Search.getFullTextEntityManager(entityManager);
-            fullTextEntityManager.createIndexer().startAndWait();
-        } catch (InterruptedException e) {
-            System.out.println("Une erreur est arrivé lors de l'indexation" + e.getMessage());
-        }
-        return;
-    }
-}
+//package com.cnesoa.config;
+//
+//import org.hibernate.search.jpa.FullTextEntityManager;
+//import org.hibernate.search.jpa.Search;
+//import org.springframework.context.ApplicationEvent;
+//import org.springframework.context.ApplicationListener;
+//import org.springframework.stereotype.Component;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.PersistenceContext;
+//
+///**
+// * Created by Maxime on 10/05/2016.
+// */
+//
+//@Component
+//public class BuildSearchIndex implements ApplicationListener{
+//
+//    @PersistenceContext
+//    private EntityManager entityManager;
+//
+//
+//    @Override
+//    public void onApplicationEvent(ApplicationEvent applicationEvent) {
+//        try {
+//            FullTextEntityManager fullTextEntityManager =
+//                    Search.getFullTextEntityManager(entityManager);
+//            fullTextEntityManager.createIndexer().startAndWait();
+//        } catch (InterruptedException e) {
+//            System.out.println("Une erreur est arrivé lors de l'indexation" + e.getMessage());
+//        }
+//        return;
+//    }
+//}
