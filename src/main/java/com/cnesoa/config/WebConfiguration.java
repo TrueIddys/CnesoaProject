f4844069ae92d7d2b3c3945e1d978291bb70aea6
Maxime PEYRAL <maxime.peyral@gmail.com>
2016-05-30 00:58:06 +0200

    no post

--- src/main/java/com/cnesoa/config/WebConfiguration.java
+++ src/main/java/com/cnesoa/config/WebConfiguration.java
@@ -22,13 +22,6 @@ import javax.sql.DataSource;
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
     @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
-    }
-
-    @Bean
     ServletRegistrationBean h2servletRegistration(){
         ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
         registrationBean.addUrlMappings("/console/*");
