b9c528b06881119a4af3863a2ff7a1242b0751df
Maxime <maxime.peyral@gmail.com>
2016-06-06 10:06:39 +0200

    mm

--- src/main/java/com/cnesoa/config/WebConfiguration.java
+++ src/main/java/com/cnesoa/config/WebConfiguration.java
@@ -15,16 +15,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableJpaRepositories
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
-<<<<<<< HEAD
-//    @Bean
-//    @Primary
-//    @ConfigurationProperties(prefix = "spring.datasource")
-//    public DataSource dataSource() {
-//        return DataSourceBuilder.create().build();
-//    }
 
-=======
->>>>>>> f4844069ae92d7d2b3c3945e1d978291bb70aea6
     @Bean
     ServletRegistrationBean h2servletRegistration(){
         ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
