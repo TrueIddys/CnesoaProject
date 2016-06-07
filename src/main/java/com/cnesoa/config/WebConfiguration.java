a1972068e4f6d10c0944cf619dd940416022dcd8
Maxime <maxime.peyral@gmail.com>
2016-06-07 15:20:44 +0200

    added postgresql properties

--- src/main/java/com/cnesoa/config/WebConfiguration.java
+++ src/main/java/com/cnesoa/config/WebConfiguration.java
@@ -1,11 +1,16 @@
 package com.cnesoa.config;
 
 import org.h2.server.web.WebServlet;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 
 /**
@@ -15,6 +20,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableJpaRepositories
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
 
     @Bean
     ServletRegistrationBean h2servletRegistration(){
